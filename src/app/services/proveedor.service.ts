4b077cfd1ef4a9d73cb9a7ef155f0660cf33c16f
Lagunita <lagunamichael2@outlook.com>
2020-05-25 13:25:46 -0600

    Login screen fix height on tiny screens

--- src/app/services/proveedor.service.ts
+++ src/app/services/proveedor.service.ts
@@ -1,8 +1,8 @@
-import { HttpClient } from '@angular/common/http'
-import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { url } from '../utils/url';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators'
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
